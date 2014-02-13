93fbd5738b385ed73e5961ac6fb18e0bd3871c5d
James McKinney <james@slashpoundbang.com>
2014-02-13 00:21:36 -0500

    add pending spec

---
+++ spec/parl_gc_ca_spec.rb
@@ -0,0 +1,8 @@
+require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
+require 'gov_kit-ca/postal_code/strategy/parl_gc_ca'
+
+describe GovKit::CA::PostalCode::Strategy::NDPCa do
+  describe '#electoral_districts' do
+    pending
+  end
+end
