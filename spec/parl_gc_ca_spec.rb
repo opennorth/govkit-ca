31c923fc45b479c7d63d2f314dcc510dca1a752e
James McKinney <james@slashpoundbang.com>
2014-03-05 18:21:58 -0500

    fix spec description

--- spec/parl_gc_ca_spec.rb
+++ spec/parl_gc_ca_spec.rb
@@ -1,7 +1,7 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 require 'gov_kit-ca/postal_code/strategy/parl_gc_ca'
 
-describe GovKit::CA::PostalCode::Strategy::NDPCa do
+describe GovKit::CA::PostalCode::Strategy::ParlGcCa do
   describe '#electoral_districts' do
     pending
   end
